de0085ce5e0b29d715fc9b3ef87dfc556d837284
rzezuch <rzezuchpl@gmail.com>
2017-01-15 19:57:18 +0100

    Nowa testowa solucja

---
+++ ConsoleApplication1/ConsoleApplication1/Linq.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class LinqExamples
+    {
+        public LinqExamples() { }
+        
+        public void Generate()
+        {
+            List<Person> personsList = new List<Person>();
+            for (int i = 0; i < 10; i++)
+            {
+                Person p;
+                if (i%2==0)
+                {
+                    p = new Person("Name " + i, i + 10, true);
+                }
+                else
+                {
+                    p = new Person("Name " + i, i + 10, false);
+                }
+                personsList.Add(p);
+            }
+
+            var someValues = personsList.Where(a => a.isAlien != null && a.isAlien == true);
+
+        }
+
+        public class Alien
+        {
+
+        }
+
+        public class Person
+        {
+            public Person() { }
+            public Person(string name, int age)
+            {
+                this.Age = age;
+                this.Name = name;
+            }
+
+            public Person(string name, int age, bool? isAlien)
+            {
+                this.Age = age;
+                this.Name = name;
+                this.isAlien = isAlien;
+            }
+
+            public int Age { get; set; }
+            public string Name { get; set; }
+            public bool? isAlien { get; set; }
+        }
+
+        public class ServicesExampleList
+        {
+            private List<Service> Services { get; set; }
+
+            public ServicesExampleList()
+            {
+                this.Services = new List<Service>();
+            }
+
+            public void AddService(Service serviceItem)
+            {
+                this.Services.Add(serviceItem);
+            }
+
+            public List<Service> GetListOfServices()
+            {
+                return this.Services;
+            }
+
+            public void GenerateLisOfService()
+            {
+                this.Services.Add(new Service
+                {
+                    DicountValue = 10,
+                    Limited = true,
+                    Price = 100,
+                    ServiceId = 1,
+                    ServiceName = "Konsulatacja internisty"
+                });
+
+                this.Services.Add(new Service
+                {
+                    DicountValue = 100,
+                    Limited = true,
+                    Price = 250,
+                    ServiceId = 2,
+                    ServiceName = "Stomatologia"
+                });
+
+                this.Services.Add(new Service
+                {
+                    DicountValue = 100,
+                    Limited = false,
+                    Price = 100,
+                    ServiceId = 3,
+                    ServiceName = "Fizjoterapia"
+                });
+
+                this.Services.Add(new Service
+                {
+                    DicountValue = 150,
+                    Limited = false,
+                    Price = 75,
+                    ServiceId = 4,
+                    ServiceName = "Konsulatacja psychologa"
+                });
+
+                this.Services.Add(new Service
+                {
+                    DicountValue = 10,
+                    Limited = false,
+                    Price = 100,
+                    ServiceId = 5,
+                    ServiceName = "Konsulatacja laryngologa"
+                });
+            }
+        }
+
+        public class Service
+        {
+            public int ServiceId { get; set; }
+            public string ServiceName { get; set; }
+            public bool Limited { get; set; }
+            public decimal DicountValue { get; set; }
+            public decimal Price { get; set; }
+        }
+
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinqExamples l = new LinqExamples();
+            //l.Generate();
+
+            LinqExamples.ServicesExampleList Services = new LinqExamples.ServicesExampleList();
+            Services.GenerateLisOfService();
+            var ListOfServices = Services.GetListOfServices();
+
+            var onlyLimited = ListOfServices.Select(x => x.Limited == true);
+            var onlyLimitedWhereClasule = ListOfServices.Where(x => x.Limited);
+
+            #region 
+            //var lista = new List<element>();
+
+
+            //lista.Add(new element() { month = 1, status = 1, year = 2016 });
+            //lista.Add(new element() { month = 1, status = 1, year = 2016 });
+            //lista.Add(new element() { month = 1, status = 1, year = 2016 });
+            //lista.Add(new element() { month = 2, status = 2, year = 2016 });
+            //lista.Add(new element() { month = 2, status = 2, year = 2016 });
+            //lista.Add(new element() { month = 2, status = 1, year = 2016 });
+
+            //foreach (var item in lista)
+            //{
+            //    Console.WriteLine(String.Format("misiac {0} rok {1} status {2}", item.month, item.year, item.status));
+            //}
+
+            //var newList1 = new List<element>();
+            
+            //    Console.WriteLine(String.Format("czy tak {0} ", lista.Where(m => m.month == 1 && m.year == 2016).All(s => s.status == 1)));
+
+            ////newList1.Add();// Select(s=>s.)
+
+
+            ////newList1.Add(lista.Where(m => m.month == 1 && m.year == 2016 && m.status == 1).ToList<element>());
+
+
+            ////foreach (var item in ele)
+            ////{
+            ////}
+
+            //foreach (var item in newList1)
+            //{
+            //    Console.WriteLine(String.Format("m {0} r {1} s {2}", item.month, item.year, item.status));
+            //}
+
+            //Console.ReadLine();
+            //#endregion
+
+            //#region interfejsy, dziedziczenie, abstrakcja
+            //VideoGraphicCard card = new VideoGraphicCard(eGraphicCardsBrands.Radeon, "HD 4250");
+            //var carname = card.GetName();
+            //PC pecet = new PC();
+            //Tv tlevision = new Tv();
+            //string template = pecet.GetProcessorName(tlevision);
+            #endregion
+
+        }
+    }
+
+    class element
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int status { get; set; }
+
+    }
+
+    interface IDeviceType
+    {
+
+    }
+
+    interface IDescription
+    {
+        string GetName();
+        string GetModel();
+    }
+
+    interface IGpu
+    {
+        decimal GpuClockSpeed();
+    }
+
+
+    public abstract class Device: IDescription
+    {
+        eGraphicCardsBrands brand;
+        string model;
+
+       public Device() { }
+
+       public Device(eGraphicCardsBrands name, string model)
+        {
+            this.model = model;
+            this.brand = name;
+        }
+
+        public string GetName()
+        {
+            return Enum.GetName(typeof(eGraphicCardsBrands), this.brand);
+        }
+
+        public string GetModel()
+        {
+            return this.model;
+        }
+    }
+
+    class PC : Device
+    {
+        public PC() { }
+
+        public string GetProcessorName(IComputers name)
+        {
+            return name.GetProcessor();
+        }
+    }
+
+    class Tv : Device, IComputers
+    {
+        string procName = "procek w telewizorze";
+        public string GetProcessor()
+        {
+            return this.procName;
+        }
+    }
+
+    public class VideoGraphicCard : Device, IGpu
+    {
+        decimal clockSpeed { get; set; }
+
+        public VideoGraphicCard() { }
+
+        public VideoGraphicCard(eGraphicCardsBrands brand, string model) : base(brand, model)
+        {
+            
+        }
+
+        public decimal GpuClockSpeed()
+        {
+            return this.clockSpeed;
+        }
+    }
+
+    public enum eGraphicCardsBrands
+    {
+        Radeon = 0,
+        Amd
+    }
+
+    interface IComputers
+    {
+        string GetProcessor();
+    }
+
+}
